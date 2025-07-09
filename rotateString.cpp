d7ce45878c89a10f88051fdd5711ca927ce82bbe
natan kassa <nkassa@usc.edu>
2025-07-08 21:33:53 -0700

    fix

--- rotateString.cpp
+++ rotateString.cpp
@@ -2,28 +2,11 @@ class Solution {
 public:
     bool rotateString(string s, string goal) 
     {
+        string newStr = s + s;
         if(s.size() != goal.size())
         {
             return false;
         }
-        int idxTwo = 0;
-        while(idxTwo < goal.size())
-        {
-            if(s[0] == goal[idxTwo])
-            {
-                break;
-            }
-            idxTwo++;
-        }
-        for(int i = 0; i < s.size(); i++)
-        {
-            idxTwo %= goal.size();
-            if(s[i] != goal[idxTwo])
-            {
-                return false;
-            }
-            idxTwo++;
-        }
-        return true;
+        return newStr.find(goal) != string::npos;
     }
 };
