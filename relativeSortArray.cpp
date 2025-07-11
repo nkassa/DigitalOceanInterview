68ae438d93ab9cf3c8417054303fb6e00100f946
natan kassa <nkassa@usc.edu>
2025-07-10 23:47:56 -0700

    relativeSortArray

---
+++ relativeSortArray.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) 
+    {
+        unordered_map<int,int> count;
+        for(int num: arr1)
+        {
+            count[num]++;
+        }
+        vector<int> ans(arr1.size());
+        int i = 0;
+        for(int num: arr2)
+        {
+            int size = count[num];
+            for(int j = 0; j < size; j++)
+            {
+                ans[i] = num;
+                i++;
+            }
+            count.erase(num);
+        }
+        for(auto [key,val]: count)
+        {
+            for(int j = 0; j < val; j++)
+            {
+                ans[i] = key;
+                i++;
+            }
+        }
+        return ans;
+    }
+};
