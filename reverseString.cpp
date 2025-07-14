f3ffcd7484c2c993fe68c25edf466dded374cf4c
natan kassa <nkassa@usc.edu>
2025-07-13 23:47:07 -0700

    reverseString

--- reverseString.cpp
+++ reverseString.cpp
@@ -2,6 +2,15 @@ class Solution {
 public:
     void reverseString(vector<char>& s) 
     {
-        reverse(s.begin(), s.end());
+        int left = 0;
+        int right = s.size()-1;
+        while(left < right)
+        {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
     }
 };
