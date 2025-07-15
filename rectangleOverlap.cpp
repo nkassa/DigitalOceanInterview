411a2321290ca66edd13f914cc206853f2adac8a
natan kassa <nkassa@usc.edu>
2025-07-14 23:45:25 -0700

    rectOverlap

--- rectangleOverlap.cpp
+++ rectangleOverlap.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) 
     {
-        int x1 = rect1[0];
-        int x2 = rect1[2];
+        int x1 = rec1[0];
+        int x2 = rec1[2];
 
-        int x3 = rect2[0];
-        int x4 = rect2[2];
+        int x3 = rec2[0];
+        int x4 = rec2[2];
 
-        int y1 = rect1[1];
-        int y2 = rect1[3];
+        int y1 = rec1[1];
+        int y2 = rec1[3];
 
-        int y3 = rect2[1];
-        int y4 = rect2[3];
+        int y3 = rec2[1];
+        int y4 = rec2[3];
 
         if(x2 <= x3 || x4 <= x1)
         {
