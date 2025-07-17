6a55c5f91e023fa8eb94565b15e957445cdce4ba
natan kassa <nkassa@usc.edu>
2025-07-17 16:06:52 -0700

    repeatedSubstringPattern

--- repeatedSubstringPattern.cpp
+++ repeatedSubstringPattern.cpp
@@ -2,24 +2,8 @@ class Solution {
 public:
     bool repeatedSubstringPattern(string s) 
     {
-        int n = s.size();
-        for(int i = 1; i <= n/2; i++)
-        {
-            if(n % i == 0)
-            {
-                string str = "";
-                string temp = s.substr(0, i);
-                for(int j = 0; j < n/i; j++)
-                {
-                    str += temp;
-                }
-                cout << str << endl;
-                if(str == s)
-                {
-                    return true;
-                }
-            }
-        }
-        return false;
+        string str = s + s;
+        int n = str.size();
+        return str.substr(1, n-2).find(s) != -1;
     }
 };
