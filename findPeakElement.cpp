f9bbf4a4608629a315ad7190965c6cb9933de7f1
natan kassa <nkassa@usc.edu>
2025-07-16 23:35:52 -0700

    fix

--- findPeakElement.cpp
+++ findPeakElement.cpp
@@ -2,14 +2,6 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums) 
     {
-        if(nums.size() == 1 || (nums.size() == 2 && nums[0] > nums[1]))
-        {
-            return 0;
-        }
-        if(nums.size() == 2 && nums[0] > nums[1])
-        {
-            return 1;
-        }
         for(int i = 1; i < nums.size()-1; i++)
         {
             if(nums[i-1] < nums[i] && nums[i] > nums[i+1])
