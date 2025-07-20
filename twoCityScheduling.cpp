ab79a6d3115d2e79bd1a3cc385dd86149e9661a8
natan kassa <nkassa@usc.edu>
2025-07-19 23:58:32 -0700

    twoCity

--- twoCityScheduling.cpp
+++ twoCityScheduling.cpp
@@ -3,9 +3,17 @@ public:
     int twoCitySchedCost(vector<vector<int>>& costs) 
     {
         int ans = 0;
+        int n = costs.size()/2;
+        vector<int> diff;
         for(vector<int> cost: costs)
         {
-            ans += min(cost[0], cost[1]);
+            ans += cost[0];
+            diff.push_back(cost[1] - cost[0]);
+        }
+        sort(diff.begin(), diff.end());
+        for(int i = 0; i < n; i++)
+        {
+            ans += diff[i];
         }
         return ans;
     }
