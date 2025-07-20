fea18d506326e1076c0a69ef7d1ab95e3865a09b
natan kassa <nkassa@usc.edu>
2025-07-19 23:48:40 -0700

    twoCity

--- nimGame.cpp
+++
@@ -1,7 +0,0 @@
-class Solution {
-public:
-    bool canWinNim(int n) 
-    {
-        return n < 3 || n % 4 != 0;
-    }
-};
--- repeatedSubstringPattern.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    bool repeatedSubstringPattern(string s) 
-    {
-        string str = s + s;
-        int n = str.size();
-        return str.substr(1, n-2).find(s) != -1;
-    }
-};
