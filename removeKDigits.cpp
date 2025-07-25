e982012de89176c0e41bcd3fdc0dc1a2db9c7229
natan kassa <nkassa@usc.edu>
2025-07-25 00:55:10 -0700

    removeKDigits

---
+++ removeKDigits.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) 
+    {
+        stack<char> st;
+        for(char c: num)
+        {
+            while(k > 0 && !st.empty() && st.top() > c)
+            {
+                st.pop();
+                k--;
+            }
+            st.push(c);
+        }
+        while(k > 0 && !st.empty())
+        {
+            st.pop();
+            k--;
+        }
+        string ans;
+        while(!st.empty())
+        {
+            ans += st.top();
+            st.pop();
+        }
+        int idx = ans.size()-1;
+        while(ans[idx] == 0)
+        {
+            idx--;
+            ans.pop_back();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
