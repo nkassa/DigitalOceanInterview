ccfae5ae20bc4980d56c4b25fdf1b454b3c660e0
natan kassa <nkassa@usc.edu>
2025-07-25 19:39:37 -0700

    divisorGame

--- divisorGame.cpp
+++ divisorGame.cpp
@@ -2,14 +2,6 @@ class Solution {
 public:
     bool divisorGame(int n) 
     {
-        if(n == 1)
-        {
-            return false;
-        }
-        if(n == 2)
-        {
-            return true;
-        }
         return n % 2 == 0;
     }
 };
