4cdc9caee42f1c1b2b866140427d3435c159114f
natan kassa <nkassa@usc.edu>
2025-07-25 20:54:13 -0700

    maxiDistToClostPers

--- maximizeDistanceToClosestPerson.cpp
+++ maximizeDistanceToClosestPerson.cpp
@@ -3,7 +3,6 @@ public:
     int maxDistToClosest(vector<int>& seats) 
     {
         vector<int> list;
-        int ans = 0;
         for(int i = 0; i < seats.size(); i++)
         {
             if(seats[i] == 1)
@@ -11,14 +10,14 @@ public:
                 list.push_back(i);
             }
         }
+        int left = 0;
+        int right = seats.size()-1;
+        int ans = list[left]-0;
+        ans = max(ans, right - list[list.size()-1]);
         for(int i = 1; i < list.size(); i++)
         {
             ans = max(ans, (list[i] - list[i-1])/2);
         }
-        int x = seats.size()-1;
-        int y = list.size()-1;
-        ans = max(ans, list[0] - 0);
-        ans = max(ans, x - list[y]);
         return ans;
     }
 };
