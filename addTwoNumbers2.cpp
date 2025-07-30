5f011ba2141aac27951f42f5aceaa1422f505f9b
natan kassa <nkassa@usc.edu>
2025-07-29 20:01:44 -0700

    addTwoNumbers2

--- addTwoNumbers2.cpp
+++ addTwoNumbers2.cpp
@@ -1,35 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
     {
-        stack<int> s1, s2;
-
-        while(l1) {
-            s1.push(l1->val);
+        string one = "";
+        string two = "";
+        while(l1)
+        {
+            one += to_string(l1->val);
             l1 = l1->next;
         }
-        while(l2) {
-            s2.push(l2->val);
+        while(l2)
+        {
+            two += to_string(l2->val);
             l2 = l2->next;
         }
 
+        ListNode* curr = new ListNode(0);
         int carry = 0;
-        ListNode* head = nullptr;
-
-        while(!s1.empty() || !s2.empty() || carry) {
+        int i = one.size()-1;
+        int j = two.size()-1;
+        while(i >= 0 || j >= 0 || carry > 0)
+        {
             int sum = carry;
-            if(!s1.empty()) {
-                sum += s1.top(); s1.pop();
+            if(i >= 0)
+            {
+                sum += one[i]-'0';
+                i--;
             }
-            if(!s2.empty()) {
-                sum += s2.top(); s2.pop();
+            if(j >= 0)
+            {
+                sum += two[j]-'0';
+                j--;
             }
+            curr->val = sum % 10;
+            ListNode* head = new ListNode(0);
+            head->next = curr;
             carry = sum / 10;
-            ListNode* newNode = new ListNode(sum % 10);
-            newNode->next = head;
-            head = newNode;
+            curr = head;
         }
-
-        return head;
+        return curr->next;
     }
 };
