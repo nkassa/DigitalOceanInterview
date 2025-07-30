edd749153b81fb0f2c07fd3e8806257238e14f05
natan kassa <nkassa@usc.edu>
2025-07-29 17:42:36 -0700

    customSortString

--- customSortString.cpp
+++ customSortString.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     string customSortString(string order, string s) 
     {
-        unordered_map<char,int> seen;
+        unordered_map<char,int> count;
         for(char c: s)
         {
-            seen[c]++;
+            count[c]++;
         }
         string ans = "";
         for(int i = 0; i < order.size(); i++)
         {
-            while(seen[order[i]] > 0)
+            while(count[order[i]] > 0)
             {
                 ans += order[i];
-                seen[order[i]]--;
+                count[order[i]]--;
             }
         }
-        for(auto [key, val]: seen)
+        for(auto [key,val]: count)
         {
-            for(int i= 0; i < val; i++)
+            for(int i = 0; i < val; i++)
             {
                 ans += key;
             }
         }
         return ans;
     }
-};
+};//
