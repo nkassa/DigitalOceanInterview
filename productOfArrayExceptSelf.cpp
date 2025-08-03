444b6b3c12fbfc78dcc8cc319dc1b000a67f9714
natan kassa <nkassa@alumni.usc.edu>
2025-08-02 23:33:15 -0700

    prodOfArrayExcepSelf

--- productOfArrayExceptSelf.cpp
+++ productOfArrayExceptSelf.cpp
@@ -2,27 +2,38 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) 
     {
-        int n = nums.size();
-        vector<int> pre(n, 1);
-        vector<int> post(n, 1);
+        vector<int> ans(nums.size(),0);
+        unordered_set<int> seen;
         int prod = 1;
+        for(int i= 0; i < nums.size();i++)
+        {
+            if(nums[i] == 0)
+            {
+                seen.insert(i);
+            }
+            else
+            {
+                prod *= nums[i];
+            }
+        }
         for(int i = 0; i < nums.size(); i++)
         {
-            prod *= nums[i];
-            pre[i] = prod;
-        }
-        prod = 1;
-        for(int i = n-1; i >= 0; i--)
-        {
-            prod *= nums[i];
-            post[i] = prod;
-        }
-        vector<int> ans(n);
-        ans[0] = post[1];
-        ans[n-1] = pre[n-2];
-        for(int i = 1; i < ans.size()-1; i++)
-        {
-            ans[i] = pre[i-1] * post[i+1];
+            if( seen.find(i) == seen.end() && !seen.empty())
+            {
+                ans[i] = 0;
+            }
+            if(seen.find(i) != seen.end() && seen.size() == 1)
+            {
+                ans[i] = prod;
+            }
+            if(seen.find(i) != seen.end() && seen.size() > 1)
+            {
+                ans[i] = 0;
+            }
+            if(seen.empty())
+            {
+                ans[i] = prod / nums[i];
+            }
         }
         return ans;
     }
