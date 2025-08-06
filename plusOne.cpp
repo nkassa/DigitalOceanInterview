aab88bb7538be360674fecd7154c2558b39f7db3
natan kassa <nkassa@alumni.usc.edu>
2025-08-05 23:52:14 -0700

    plusOne

--- plusOne.cpp
+++ plusOne.cpp
@@ -5,17 +5,26 @@ public:
         int n = digits.size();
         int carry = 0;
         vector<int> ans(n+1, 0);
-        for(int i = n-1; i >= 0; i--)
+        int i = n-1;
+        while(i >= 0 || carry > 0)
         {
-            int sum = digits[i] + carry;
-            if(i == n-1)
+            if(i >= 0)
             {
-                sum += 1;
+                int sum = digits[i] + carry;
+                if(i == n-1)
+                {
+                    sum += 1;
+                }
+                ans[i+1] = sum % 10;
+                carry = sum / 10;
             }
-            ans[i+1] = sum % 10;
-            carry = sum / 10;
+            else
+            {
+                ans[0] = carry;
+                carry = 0;
+            }
+            i--;
         }
-        ans[0] = carry;
         if(ans[0] == 0)
         {
             vector<int> sol;
