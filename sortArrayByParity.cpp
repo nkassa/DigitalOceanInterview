2137a4e3a614b99fb6ee70530c0671f8d00d84cb
natan kassa <nkassa@alumni.usc.edu>
2025-08-06 23:53:34 -0700

    sortArrayByParity

--- sortArrayByParity.cpp
+++ sortArrayByParity.cpp
@@ -7,18 +7,20 @@ public:
         int right = n-1;
         while(left < right)
         {
-            if(nums[left] % 2 > nums[right] % 2)
-            {
-                int temp = nums[left];
-                nums[left] = nums[right];
-                nums[right] = temp;
-            }
             if(nums[left] % 2 == 0)
             {
                 left++;
             }
-            if(nums[right] % 2 != 0)
+            else if(nums[right] % 2 != 0)
             {
+                right--;
+            }
+            else
+            {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
                 right--;
             }
         }
