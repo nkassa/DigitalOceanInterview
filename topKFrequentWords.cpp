ce9d8171565be7cdb2b1038d4375ce4a50fc4a34
natan kassa <nkassa@alumni.usc.edu>
2025-08-13 00:57:40 -0700

    topKFreqWord

---
+++ topKFrequentWords.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) 
+    {
+        unordered_map<string,int> count;
+        for(string word: words)
+        {
+            count[word]++;
+        }
+        priority_queue<pair<int,string>> heap;
+        for(auto [key,val]: count)
+        {
+            int x = -1 * val;
+            heap.push({x, key});
+        }
+        if(heap.size() > k)
+        {
+            heap.pop();
+        }
+        vector<string> ans(k);
+        int i = k-1;
+        while(!heap.empty())
+        {
+            ans[i] = heap.top().second;
+            i--;
+            heap.pop();
+        }
+        return ans;
+    }
+};
