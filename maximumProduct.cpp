b0af4b820801852cf708be657e97f3088ba06887
natan kassa <nkassa@alumni.usc.edu>
2025-08-12 23:56:16 -0700

    maxProd

--- maximumProduct.cpp
+++ maximumProduct.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     int maximumProduct(vector<int>& nums) 
     {
+        int n = nums.size();
         sort(nums.begin(), nums.end());
         int ans = 1;
-        int i = nums.size()-1;
+        int i = n-1;
         int cnt = 0;
         while(i >= 0 && cnt < 3)
         {
@@ -12,7 +13,7 @@ public:
             cnt++;
             i--;
         }
-        ans = max(ans, (nums[0]*nums[1]*nums[nums.size()-1]));
+        ans = max(ans, (nums[0]*nums[1]*nums[n-1]));
         return ans;
     }
 };
