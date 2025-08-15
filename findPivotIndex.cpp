d1b8ad8419d212528e43b408a73cd930113c4316
natan kassa <nkassa@alumni.usc.edu>
2025-08-15 14:28:12 -0700

    findPivotIndex

--- findPivotIndex.cpp
+++ findPivotIndex.cpp
@@ -4,32 +4,25 @@ public:
     {
         int n = nums.size();
         vector<int> prefix(n,0);
-        vector<int> suffix(n,0);
         prefix[0] = nums[0];
-        suffix[n-1] = nums[n-1];
         for(int i = 1; i < n; i++)
         {
             prefix[i] = prefix[i-1] + nums[i];
         }
-
-        for(int i = n-2; i >= 0; i--)
-        {
-            suffix[i] = suffix[i+1] + nums[i];
-        }
-        if(suffix[0] - nums[0] == 0)
+        if(prefix[n-1] - prefix[0] == 0)
         {
             return 0;
         }
         for(int i = 1; i < n-1; i++)
         {
             int left = prefix[i-1];
-            int right = suffix[i+1];
+            int right = prefix[n-1] - prefix[i];
             if(left == right)
             {
                 return i;
             }
         }
-        if(prefix[n-1] - nums[n-1] == 0)
+        if(prefix[n-2] == 0)
         {
             return n-1;
         }
