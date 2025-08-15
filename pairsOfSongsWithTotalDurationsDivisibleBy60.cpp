52c76632cf55075a6b4bcd74eb858a728ad770de
natan kassa <nkassa@alumni.usc.edu>
2025-08-14 23:04:35 -0700

    pairsOfSongsWithTotalDurationsDivBy60

--- pairsOfSongsWithTotalDurationsDivisibleBy60.cpp
+++ pairsOfSongsWithTotalDurationsDivisibleBy60.cpp
@@ -2,17 +2,14 @@ class Solution {
 public:
     int numPairsDivisibleBy60(vector<int>& time) 
     {
+        vector<int> count(60, 0); // count of each remainder
         int ans = 0;
-        for(int i = 0; i < time.size()-1; i++)
-        {
-            for(int j = i + 1; j < time.size(); j++)
-            {
-                int sum = time[i] + time[j];
-                if(sum % 60 == 0)
-                {
-                    ans++;
-                }
-            }
+
+        for (int t : time) {
+            int rem = t % 60;
+            int complement = (60 - rem) % 60; // handles rem = 0 case
+            ans += count[complement]; // add how many we can pair with
+            count[rem]++; // store remainder for future matches
         }
         return ans;
     }
