fd2201418291db599fafae117f16879de4efcf03
natan kassa <nkassa@alumni.usc.edu>
2025-08-16 13:09:31 -0700

    wordPattern

--- wordPattern.cpp
+++ wordPattern.cpp
@@ -2,42 +2,33 @@ class Solution {
 public:
     bool wordPattern(string pattern, string s) 
     {
-        vector<string> list;
+        vector<string> str;
+        unordered_map<char,string> count;
+        unordered_map<string,char> countTwo;
         stringstream ss(s);
         string temp = "";
-
         while(getline(ss, temp, ' '))
         {
-            list.push_back(temp);
+            str.push_back(temp);
         }
-
-        if(pattern.size() != list.size())
+        if(pattern.size() != str.size())
         {
             return false;
         }
-
-        unordered_map<char, string> count;
-        unordered_map<string, char> countTwo;
-        
         for(int i = 0; i < pattern.size(); i++)
         {
-            if(count.find(pattern[i]) != count.end())
-            {
-                if(count[pattern[i]] != list[i])
-                {
-                    return false;
-                }
-            }
-            if(countTwo.find(list[i]) != countTwo.end())
-            {
-                if(countTwo[list[i]] != pattern[i])
-                {
-                    return false;
-                }
-            }
-            count[pattern[i]] = list[i];
-            countTwo[list[i]] = pattern[i];
+            count[pattern[i]] = str[i];
+            countTwo[str[i]] = pattern[i];
         }
-        return true;
+        string check = "";
+        string checkTwo = "";
+        for(int i = 0; i < pattern.size(); i++)
+        {
+            check += count[pattern[i]];
+            check += " ";
+            checkTwo += countTwo[str[i]];
+        }
+        check.pop_back();
+        return s == check && pattern == checkTwo;
     }
 };
