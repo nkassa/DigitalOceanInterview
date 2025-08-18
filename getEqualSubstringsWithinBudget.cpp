9be712cc4f87a051a2cd42cd31ebd2b16e530ccd
natan kassa <nkassa@alumni.usc.edu>
2025-08-17 23:46:31 -0700

    getEqualSubstrWithinBudget

--- getEqualSubstringsWithinBudget.cpp
+++ getEqualSubstringsWithinBudget.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
     int equalSubstring(string s, string t, int maxCost) 
     {
-        int sum = 0;
+        int curr = 0;
         int left = 0;
         int ans = 0;
         for(int right = 0; right < s.size(); right++)
         {
-            sum += abs(s[right] - t[right]);
-            while(sum > maxCost && left <= right)
+            curr += abs(s[right] - t[right]);
+            while(curr > maxCost)
             {
-                sum -= abs(s[left] - t[left]);
+                curr -= abs(s[left] - t[left]);
                 left++;
             }
             ans = max(ans, right-left+1);
         }
         return ans;
+        
     }
 };
