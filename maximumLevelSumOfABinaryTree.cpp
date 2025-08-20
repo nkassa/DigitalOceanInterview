80f3f2dc63994e6a4d5584599185c75ae14f466e
natan kassa <nkassa@alumni.usc.edu>
2025-08-19 23:46:55 -0700

    maxLevelSumOfABinaryTree

---
+++ maximumLevelSumOfABinaryTree.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) 
+    {
+        int level = 1;
+        int maxi = 0;
+        int curr = 0;
+        int ans = 0;
+        queue<TreeNode*> queue;
+        queue.push(root);
+        while(!queue.empty())
+        {
+            int size = queue.size();
+            for(int i = 0; i < size; i++)
+            {
+                TreeNode* node = queue.front();
+                curr += node->val;
+                queue.pop();
+                if(node->left)
+                {
+                    queue.push(node->left);
+                }
+                if(node->right)
+                {
+                    queue.push(node->right);
+                }
+            }
+            level++;
+            if(maxi  < curr)
+            {
+                maxi = curr;
+                ans = level;
+            }
+        }
+        return ans;
+    }
+};
