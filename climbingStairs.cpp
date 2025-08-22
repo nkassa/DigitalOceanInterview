968ae09a9b51da098e4627592c614f121040edc9
natan kassa <nkassa@alumni.usc.edu>
2025-08-21 18:29:01 -0700

    fix

--- climbingStairs.cpp
+++ climbingStairs.cpp
@@ -3,23 +3,18 @@ public:
     vector<int> memo;
     int climbStairs(int n) 
     {
-        if(n <= 3)
-        {
-            return n;
-        }
         memo = vector<int>(n+1, -1);
+        memo[0] = 1;
         memo[1] = 1;
-        memo[2] = 2;
         return dp(n);
     }
-    int dp(int i)
+    int dp(int idx)
     {
-        if(memo[i] != -1)
+        if(memo[idx] != -1)
         {
-            return memo[i];
+            return memo[idx];
         }
-        memo[i] = dp(i-1) + dp(i-2);
-        return memo[i];
+        memo[idx] = dp(idx-1) + dp(idx-2);
+        return memo[idx];
     }
 };
-//rec
