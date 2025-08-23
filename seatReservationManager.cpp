edd089093e64dcf28ba9ba64dcb6df95dc895815
natan kassa <nkassa@alumni.usc.edu>
2025-08-22 23:55:06 -0700

    seatResMan

---
+++ seatReservationManager.cpp
@@ -0,0 +1,30 @@
+class SeatManager {
+public:
+    priority_queue<int, vector<int>, greater<int>> heap;
+    SeatManager(int n) 
+    {
+        for(int i = 1; i <= n; i++)
+        {
+            heap.push(i);
+        }
+    }
+    
+    int reserve() 
+    {
+        int result = heap.top();
+        heap.pop();
+        return result;
+    }
+    
+    void unreserve(int seatNumber) 
+    {
+        heap.push(seatNumber);
+    }
+};
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * SeatManager* obj = new SeatManager(n);
+ * int param_1 = obj->reserve();
+ * obj->unreserve(seatNumber);
+ */
