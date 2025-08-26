197cde0b2751bfa0e421b757c5da2dac62ab8796
natan kassa <nkassa@alumni.usc.edu>
2025-08-26 00:39:07 -0700

    fix

--- splitArrayLargestSum.cpp
+++ splitArrayLargestSum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int splitArray(vector<int>& nums, int k) 
     {
-        int left = 1;
+        int left = 0;
         int right = 0;
         for(int num: nums)
         {
@@ -24,17 +24,24 @@ public:
     }
     bool check(int mid, vector<int>& nums, int& k)
     {
-        int cnt = 0;
+        int cnt = 1;
         int sum = 0;
         for(int i= 0; i < nums.size(); i++)
         {
-            sum += nums[i];
-            if(sum >= mid)
+            if(sum + nums[i] > mid)
             {
                 cnt++;
-                sum = 0;
+                sum = nums[i];
+                if(cnt > k)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                sum += nums[i];
             }
         }
-        return cnt == k;
+        return true;
     }
 };
