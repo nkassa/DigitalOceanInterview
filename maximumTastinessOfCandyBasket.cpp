3d27beac2b7f2029c83f8c02b72ed8e9d9be2acf
natan kassa <nkassa@alumni.usc.edu>
2025-08-27 23:14:11 -0700

    maxTastinessOfCandyBasket

---
+++ maximumTastinessOfCandyBasket.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int maximumTastiness(vector<int>& price, int k) 
+    {
+        int left = 0;
+        int right = 0;
+        for(int num: price)
+        {
+            right = max(num, right);
+        }
+        while(left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if(check(mid, price, k))
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+        return right;
+    }
+    bool check(int mid, vector<int>& price, int& k)
+    {
+        int cnt = 0;
+        int lastIdx = 0;
+        for(int i = 0; i < price.size(); i++)
+        {
+            if(abs(price[i] - price[lastIdx]) >= mid)
+            {
+                cnt++;
+                lastIdx = i;
+                if(cnt >= k)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
