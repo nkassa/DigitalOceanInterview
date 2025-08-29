93b61914927b42a17d1b226aa4db4139087b436b
natan kassa <nkassa@alumni.usc.edu>
2025-08-29 00:01:45 -0700

    fix

--- sortColors.cpp
+++ sortColors.cpp
@@ -1,27 +1,6 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) 
-    {
-        unordered_map<int,int> count;
-        int left = 0;
-        for(int num: nums)
-        {
-            count[num]++;
-        }
-        for(int i = 0; i < count[0]; i++)
-        {
-            nums[left] = 0;
-            left++;
-        }
-        for(int i = 0; i < count[1]; i++)
-        {
-            nums[left] = 1;
-            left++;
-        }
-        for(int i = 0; i < count[2]; i++)
-        {
-            nums[left] = 2;
-            left++;
-        }
+    void sortColors(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
     }
 };
