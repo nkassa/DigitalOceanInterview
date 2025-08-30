cb2458dd2d9fbdd38e19b582274d686db0eda11c
natan kassa <nkassa@alumni.usc.edu>
2025-08-29 17:36:45 -0700

    fix

--- productOfArrayExceptSelf.cpp
+++ productOfArrayExceptSelf.cpp
@@ -3,21 +3,22 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) 
     {
         int n = nums.size();
-        vector<int> ans(n,1);
-        int pre = 1;
-        for(int i = 0; i < n; i++)
+        vector<int> prefix(n);
+        vector<int> suffix(n);
+        prefix[0] = nums[0];
+        suffix[n-1] = nums[n-1];
+        for(int i = 1; i < nums.size(); i++)
         {
-            ans[i] = pre;
-            pre *= nums[i];
+            prefix[i] = prefix[i-1] * nums[i];
+            suffix[n-1-i] = suffix[n-i] * nums[n-1-i];
         }
-        pre = 1;
-        for(int i = n-1; i >= 0; i--)
+        vector<int> ans(n);
+        ans[0] = suffix[1];
+        ans[n-1] = prefix[n-2];
+        for(int i = 1; i < n-1; i++)
         {
-            ans[i] *= pre;
-            pre *= nums[i];
+            ans[i] = prefix[i-1] * suffix[i+1];
         }
         return ans;
     }
 };
-// 1 
-// -1 -1 0 0 0
