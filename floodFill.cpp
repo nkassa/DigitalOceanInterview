c97e2308eef164317d78f13b66420f23ef4f8675
natan kassa <nkassa@alumni.usc.edu>
2025-08-30 23:46:46 -0700

    fix

--- floodFill.cpp
+++ floodFill.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    int m;
-    int n;
     vector<vector<int>> directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
     vector<vector<bool>> seen;
-    vector<vector<int>> image;
+    vector<vector<int>> ima;
+    int m;
+    int n;
     int color;
     int num;
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) 
     {
         m = image.size();
         n = image[0].size();
-        this->image = image;
+        this->ima = image;
         this->color = color;
         seen = vector(m, vector<bool>(n, false));
         seen[sr][sc] = true;
         num = image[sr][sc];
-        image[sr][sc] = color;
-        dfs(sr, sc, image);
-        return image;
+        ima[sr][sc] = color;
+        dfs(sr, sc);
+        return ima;
     }
-    void dfs(int row, int col, vector<vector<int>>& image)
+    void dfs(int row, int col)
     {
         for(vector<int> direction: directions)
         {
@@ -29,13 +29,13 @@ public:
             if(valid(nextRow, nextCol) && !seen[nextRow][nextCol])
             {
                 seen[nextRow][nextCol] = true;
-                image[nextRow][nextCol] = color;
-                dfs(nextRow, nextCol, image);
+                ima[nextRow][nextCol] = color;
+                dfs(nextRow, nextCol);
             }
         }
     }
     bool valid(int row, int col)
     {
-        return 0 <= row && row < m && 0 <= col && col < n && image[row][col] == num;
+        return 0 <= row && row < m && 0 <= col && col < n && ima[row][col] == num;
     }
 };
